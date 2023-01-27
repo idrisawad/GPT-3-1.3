cebd8ea02edbfe7f678123d5b840815c98ef455a
Idris Awad <123701734+idrisawad@users.noreply.github.com>
2023-01-27 18:54:19 +0100

    Update gpt-3-1.3.py
    In this code, I've added two new methods, save_model and load_model, which allow you to save and load the fine-tuned model, optimizer, and scheduler state, respectively. The save_model method takes the path where the model should be saved, and load_model takes the path where the model is saved.

--- gpt-3-1.3.py
+++ gpt-3-1.3.py
@@ -3,10 +3,11 @@ import random
 import string
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, AdamW
 import torch
-
-nltk.download('punkt')
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+import torch.nn as nn
+from torch.optim import Optimizer
 
 class DeepLearningChatbot:
 
@@ -16,10 +17,26 @@ class DeepLearningChatbot:
         self.sentences = []
         self.model = None
         self.tokenizer = None
+        self.history = []
+        self.optimizer = None
+        self.scheduler = None
+        self.loss_fn = None
+        self.device = None
+        self.batch_size = None
 
     def learn_from_internet(self, text):
         self.sentences = nltk.sent_tokenize(text)
 
+    def _normalize(self, text):
+        return nltk.word_tokenize(text.lower().translate
+
+
+
+Idris Awad
+continue here
+Sure, here is the continued code:
+
+Copy code
     def _normalize(self, text):
         return nltk.word_tokenize(text.lower().translate(self.remove_punct_dict))
 
@@ -35,34 +52,82 @@ class DeepLearningChatbot:
 
         return response_text
 
-    def fine_tune_gpt3(self, model_name, train_texts, train_labels):
+    def fine_tune_gpt3(self, model_name, train_texts, train_labels, batch_size=4, device='cuda'):
+        self.batch_size = batch_size
+        self.device = device
+        
         # Instantiate the model and tokenizer
-        self.model = AutoModelForCausalLM.from_pretrained(model_name)
+        self.model = AutoModelForCausalLM.from_pretrained(model_name).to(device)
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        
+        # Freeze some layers
+        for name, param in self.model.named_parameters():
+            if 'layer.' not in name:
+                param.requiresGrad = False
 
         # Prepare the data for fine-tuning
         input_ids = torch.tensor([self.tokenizer.encode(text) for text in train_texts])
         labels = torch.tensor(train_labels)
 
+        # Initialize the optimizer and scheduler
+        self.optimizer = AdamW(filter(lambda p: p.requires_grad, self.model.parameters()), lr=5e-5)
+        self.scheduler = ReduceLROnPlateau(self.optimizer, patience=3, verbose=True)
+
+
+
+Idris Awad
+continue here
+Sure, here is the continued code:
+
+Copy code
+    def fine_tune_gpt3(self, model_name, train_texts, train_labels, batch_size=4, device='cuda'):
+        # ...
+        self.loss_fn = nn.CrossEntropyLoss()
+
         # Fine-tune the model
-        self.model.fine_tune(input_ids, labels)
+        for epoch in range(10):
+            for i in range(0, len(input_ids), batch_size):
+                input_batch = input_ids[i:i + batch_size].to(device)
+                labels_batch = labels[i:i + batch_size].to(device)
+
+                self.optimizer.zero_grad()
+                logits = self.model(input_batch)[0]
+                loss = self.loss_fn(logits.view(-1, logits.size(-1)), labels_batch.view(-1))
+                loss.backward()
+                self.optimizer.step()
+
+            self.scheduler.step(loss)
+            print("epoch", epoch, "loss", loss.item())
 
     def chat(self):
         user_input = input("You: ")
         while user_input != 'bye':
+            self.history.append(user_input)
             response = self._get_response(user_input)
             print("Chatbot: ", response)
             self.sentences.append(user_input)
             user_input = input("You: ")
         print("Chatbot: Bye!")
-
-#initialize the chatbot
-chatbot = DeepLearningChatbot()
-
-# Learn from the internet
-chatbot.learn_from_internet("Insert here the text you want the chatbot to learn from the internet")
-
-# Fine-tune GPT-3 on the chatbot's learned data
-chatbot.fine_tune_gpt3("gpt3", chatbot.sentences, [0] * len(chatbot.sentences))
-#start chatting
-chatbot.chat()
+def chat(self):
+        user_input = input("You: ")
+        while user_input != 'bye':
+            self.history.append(user_input)
+            response = self._get_response(user_input)
+            print("Chatbot: ", response)
+            self.sentences.append(user_input)
+            user_input = input("You: ")
+        print("Chatbot: Bye!")
+        
+    def save_model(self, path):
+        torch.save({
+            'state_dict': self.model.state_dict(),
+            'optimizer': self.optimizer.state_dict(),
+            'scheduler': self.scheduler.state_dict()
+        }, path)
+        
+    def load_model(self, path):
+        checkpoint = torch.load(path)
+        self.model.load_state_dict(checkpoint['state_dict'])
+        self.optimizer.load_state_dict(checkpoint['optimizer'])
+        self.scheduler.load_state_dict(checkpoint['scheduler'])
+        self.model.to(self.device)
