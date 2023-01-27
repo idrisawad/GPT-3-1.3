eaaa30d32bcc1916405becbfb40ba3687e364ca9
Idris Awad <123701734+idrisawad@users.noreply.github.com>
2023-01-27 19:02:57 +0100

    Update gpt-3-1.3.py
    Improved,  the use of open-source datasets and automated scraping for fine-tuning the GPT-3 model in the Deep Learning Chatbot

--- gpt-3-1.3.py
+++ gpt-3-1.3.py
@@ -8,6 +8,7 @@ import torch
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 import torch.nn as nn
 from torch.optim import Optimizer
+import scrapy
 
 class DeepLearningChatbot:
 
@@ -28,16 +29,6 @@ class DeepLearningChatbot:
         self.sentences = nltk.sent_tokenize(text)
 
     def _normalize(self, text):
-        return nltk.word_tokenize(text.lower().translate
-
-
-
-Idris Awad
-continue here
-Sure, here is the continued code:
-
-Copy code
-    def _normalize(self, text):
         return nltk.word_tokenize(text.lower().translate(self.remove_punct_dict))
 
     def _lemmatize(self, word):
@@ -52,82 +43,68 @@ Copy code
 
         return response_text
 
-    def fine_tune_gpt3(self, model_name, train_texts, train_labels, batch_size=4, device='cuda'):
-        self.batch_size = batch_size
-        self.device = device
-        
-        # Instantiate the model and tokenizer
-        self.model = AutoModelForCausalLM.from_pretrained(model_name).to(device)
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
-        
-        # Freeze some layers
-        for name, param in self.model.named_parameters():
-            if 'layer.' not in name:
-                param.requiresGrad = False
+    def load_dataset(self, file_path):
+        texts = []
+        labels = []
+        with open(file_path, 'r') as f:
+            for line in f:
+                data = line.strip().split("\t")
+                texts.append(data[0])
+                labels.append(data[1])
+        return texts, labels
 
-        # Prepare the data for fine-tuning
-        input_ids = torch.tensor([self.tokenizer.encode(text) for text in train_texts])
-        labels = torch.tensor(train_labels)
-
-        # Initialize the optimizer and scheduler
-        self.optimizer = AdamW(filter(lambda p: p.requires_grad, self.model.parameters()), lr=5e-5)
-        self.scheduler = ReduceLROnPlateau(self.optimizer, patience=3, verbose=True)
+    def fine_tune_gpt3(self, model_name, dataset_path, batch_size
 
 
 
 Idris Awad
-continue here
-Sure, here is the continued code:
+ continue here
+=4, device='cuda'):
+self.batch_size = batch_size
+self.device = device
 
 Copy code
-    def fine_tune_gpt3(self, model_name, train_texts, train_labels, batch_size=4, device='cuda'):
-        # ...
-        self.loss_fn = nn.CrossEntropyLoss()
+    # Instantiate the model and tokenizer
+    self.model = AutoModelForCausalLM.from_pretrained(model_name).to(device)
+    self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+    
+    # Freeze some layers
+    for name, param in self.model.named_parameters():
+        if 'layer.' not in name:
+            param.requiresGrad = False
 
-        # Fine-tune the model
-        for epoch in range(10):
-            for i in range(0, len(input_ids), batch_size):
-                input_batch = input_ids[i:i + batch_size].to(device)
-                labels_batch = labels[i:i + batch_size].to(device)
+    # Prepare the data for fine-tuning
+    train_texts, train_labels = self.load_dataset(dataset_path) # <--- Fill the dataset path here
+    input_ids = torch.tensor([self.tokenizer.encode(text) for text in train_texts])
+    labels = torch.tensor(train_labels)
 
-                self.optimizer.zero_grad()
-                logits = self.model(input_batch)[0]
-                loss = self.loss_fn(logits.view(-1, logits.size(-1)), labels_batch.view(-1))
-                loss.backward()
-                self.optimizer.step()
+    # Initialize the optimizer and scheduler
+    self.optimizer = AdamW(filter(lambda p: p.requires_grad, self.model.parameters()), lr=5e-5)
+    self.scheduler = ReduceLROnPlateau(self.optimizer, patience=3, verbose=True)
+    self.loss_fn = nn.CrossEntropyLoss()
 
-            self.scheduler.step(loss)
-            print("epoch", epoch, "loss", loss.item())
+    # Fine-tune the model
+    for epoch in range(10):
+        for i in range(0, len(input_ids), batch_size):
+            input_batch = input_ids[i:i + batch_size].to(device)
+            labels_batch = labels[i:i + batch_size].to(device)
 
-    def chat(self):
-        user_input = input("You: ")
-        while user_input != 'bye':
-            self.history.append(user_input)
-            response = self._get_response(user_input)
-            print("Chatbot: ", response)
-            self.sentences.append(user_input)
-            user_input = input("You: ")
-        print("Chatbot: Bye!")
-def chat(self):
-        user_input = input("You: ")
-        while user_input != 'bye':
-            self.history.append(user_input)
-            response = self._get_response(user_input)
-            print("Chatbot: ", response)
-            self.sentences.append(user_input)
-            user_input = input("You: ")
-        print("Chatbot: Bye!")
-        
-    def save_model(self, path):
-        torch.save({
-            'state_dict': self.model.state_dict(),
-            'optimizer': self.optimizer.state_dict(),
-            'scheduler': self.scheduler.state_dict()
-        }, path)
-        
-    def load_model(self, path):
-        checkpoint = torch.load(path)
-        self.model.load_state_dict(checkpoint['state_dict'])
-        self.optimizer.load_state_dict(checkpoint['optimizer'])
-        self.scheduler.load_state_dict(checkpoint['scheduler'])
-        self.model.to(self.device)
+            self.optimizer.zero_grad()
+            logits = self.model(input_batch)[0]
+            loss = self.loss_fn(logits.view(-1, logits.size(-1)), labels_batch.view(-1))
+            loss.backward()
+            self.optimizer.step()
+
+        self.scheduler.step(loss)
+
+class CodeSpider(scrapy.Spider):
+    name = "code"
+    start_urls = [
+        'https://stackoverflow.com/questions/tagged/python',
+        'https://github.com/search?q=python+code+snippet&type=Code'
+    ]
+    def parse(self, response):
+        for code_snippet in response.css('pre'):
+            yield {'code': code_snippet.css('::text').get()}
+            with open("code_snippets.txt", "a") as f:
+                f.write(code_snippet.css('::text').get() + "\n")
